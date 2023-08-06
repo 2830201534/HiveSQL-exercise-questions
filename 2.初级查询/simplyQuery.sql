58927aed21b8e0e7b60d9a7c6dd70b5176a6aec0
hejie <2830201534@qq.com>
2023-08-06 20:25:40 +0800

    update project

--- 2.初级查询/simplyQuery.sql
+++ 2.初级查询/simplyQuery.sql
@@ -1,10 +1,8 @@
 -- 1.查询姓名中带“山”的学生名单
 select * from student_info where stu_name like "%山%";
 
-
 -- 2.查询姓“王”老师的个数
 select count(1) cnt from teacher_info where tea_name like "%王%";
-
 
 -- 3.检索课程编号为“04”且分数小于60的学生的分数信息，结果按分数降序排列
 select
@@ -23,7 +21,6 @@ where
     ci.course_id="04" and si.score < 60
 order by
     score desc ;
-
 
 -- 4.查询数学成绩不及格(<60)的学生信息和其对应的数学学科成绩，按照学号升序排序
 select
